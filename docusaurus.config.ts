1e8113931d2b5e86fdfb461c041f569b15dde34d
Sam Gutentag <1404219+samgutentag@users.noreply.github.com>
2024-09-20 11:37:22 -0700

    hackathon banner update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -56,7 +56,7 @@ const config: Config = {
     announcementBar: {
       id: "amplify_hackathon",
       content:
-        '👩‍💻 <a target="_blank" rel="noopener noreferrer" href="https://www.audius.events/e/hackathon">Join the Amplify Hackathon!</a> 🚀   Registration Open September 23-30, 2024',
+        '👩‍💻 <a target="_blank" rel="noopener noreferrer" href="https://www.audius.events/e/hackathon">Join the Amplify Hackathon! September 23-30, 2024</a>🚀',
       backgroundColor: "#7e1bcc",
       textColor: "#fff",
       isCloseable: false,
